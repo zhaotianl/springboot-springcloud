30e25f487d42465f56606428f2817c6fb55e5e82
tomom_glo <314903044@qq.com>
2019-03-26 11:10:37 +0800

    onice

---
+++ microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaServer7001_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001_App.class, args);
+    }
+}
