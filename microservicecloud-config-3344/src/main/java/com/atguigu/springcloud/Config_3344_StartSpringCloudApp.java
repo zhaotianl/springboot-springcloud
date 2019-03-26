999ae7376348c96b79651083b4dd5039a8dd72fc
tomom_glo <314903044@qq.com>
2019-03-26 11:00:15 +0800

    onice

---
+++ microservicecloud-config-3344/src/main/java/com/atguigu/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class,args);
+    }
+
+}
