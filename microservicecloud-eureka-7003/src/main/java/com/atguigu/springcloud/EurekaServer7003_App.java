e2f8cb115d8708594c36b4cf315e7f5b589610bd
zhaotianl <43660884+zhaotianl@users.noreply.github.com>
2019-05-14 14:38:00 +0800

    Update EurekaServer7003_App.java

--- microservicecloud-eureka-7003/src/main/java/com/atguigu/springcloud/EurekaServer7003_App.java
+++ microservicecloud-eureka-7003/src/main/java/com/atguigu/springcloud/EurekaServer7003_App.java
@@ -10,5 +10,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServer7003_App {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer7003_App.class, args);
+        //测试的测试的的的的的的
     }
 }
